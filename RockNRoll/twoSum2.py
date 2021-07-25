6a602387f979c889461442bccd83e2f17a4ca111
Hitesh <hitesh.135288@gmail.com>
2021-07-25 18:21:52 +0530

    167. Two Sum II - Input array is sorted

--- RockNRoll/twoSum2.py
+++ RockNRoll/twoSum2.py
@@ -1,3 +1,7 @@
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+# Runtime: 60 ms, faster than 85.59% of Python3 online submissions for Two Sum II - Input array is sorted.
+# Memory Usage: 14.8 MB, less than 32.26% of Python3 online submissions for Two Sum II - Input array is sorted.
 def twoSum2(nums: [int], target: int) -> [int]:
     left = 0
     right = len(nums) - 1
@@ -8,7 +12,7 @@ def twoSum2(nums: [int], target: int) -> [int]:
         elif nums[left]+nums[right] < target:
             left += 1
         else:
-            return [left, right]
+            return [left+1, right+1]
     return
 
 print(twoSum2([2,7,11,15], 9))
